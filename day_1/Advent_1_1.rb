37eb26d47d8da2b13bcd8da16fac0576fa7b4b19
cjsmi <ClarenceSmith90@gmail.com>
2022-12-05 06:09:45 -0500

    Day 1 complete

---
+++ day_1/Advent_1_1.rb
@@ -0,0 +1,29 @@
+file = File.open("data.txt")
+
+file_data = file.read
+
+file_data = file_data.split("\r\n\r\n")
+
+file_data = file_data.map do |ele| 
+    split = ele.split("\r\n")
+    split.map {|ele| ele.to_i}
+end
+
+sums = file_data.map {|ele| ele.sum}
+
+# max = file_data.max {|ele| ele.sum}
+
+# p file_data
+p sums.count
+first = sums.delete(sums.max)
+second  = sums.delete(sums.max)
+third = sums.delete(sums.max)
+
+p first + second + third
+# split_data = file_data.split("\n\n")
+
+# elves = split_data.map do |elve|
+#     elve.split("\n")
+# end
+
+# print elves
