c9a9e4188cfa4c2a670f5db9448447a34896adaf
cjsmi <ClarenceSmith90@gmail.com>
2023-01-13 17:05:57 -0500

    complete day 14 with python

--- day_14_python/Advent_14_2.py
+++ day_14_python/Advent_14_2.py
@@ -16,6 +16,7 @@ def cleanup(line):
 file_data = list(map(cleanup, data))
 
 walls = set()
+lowest = 0
 
 for line in file_data:
     reading = line[:]
@@ -30,9 +31,13 @@ for line in file_data:
             if last[1] < pos[1]:
                 for y in range(last[1], pos[1]+1):
                     walls.add(tuple([last[0], y]))
+                    if y > lowest:
+                        lowest = y
             elif last[1] > pos[1]:
                 for y in range(pos[1], last[1]+1):
                     walls.add(tuple([last[0], y]))
+                    if y > lowest:
+                        lowest = y
         
         if last[1] == pos[1]:
             if last[0] < pos[0]:
@@ -43,6 +48,12 @@ for line in file_data:
                     walls.add(tuple([x, last[1]]))
         
         last = pos
+
+lowest += 2
+
+for x in range(0, 1000):
+    walls.add((x, lowest))
+# print(lowest)
 # print(len(walls))
 class Sand:
     count = 0
@@ -55,9 +66,9 @@ class Sand:
         steps = 0
         while self.falling:
             steps += 1
-            if steps > 750:
-                # print("Fallen")
-                return False
+            # if steps > 750:
+            #     # print("Fallen")
+            #     return False
             if tuple([self.pos[0], self.pos[1]+1]) not in walls:
                 self.pos[1] += 1
             elif tuple([self.pos[0]-1, self.pos[1]+1]) not in walls:
@@ -69,10 +80,13 @@ class Sand:
             else:
                 self.falling = False
                 walls.add(tuple(self.pos))
+                if self.pos == [500, 0]:
+                    return False
                 # print("Rest")
-                # print(Sand.count)
+                print(Sand.count)
                 # print(self.pos)
                 # print()
+                print(self.pos)
                 return True
         
 sand = Sand()
@@ -81,5 +95,5 @@ sand = Sand()
 while sand.drop(walls):
     sand = Sand()
 
-print(Sand.count - 1)
+print(Sand.count)
 
