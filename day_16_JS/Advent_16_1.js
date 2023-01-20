039b798e9840042576073b7ea624db443f7369e1
cjsmi <ClarenceSmith90@gmail.com>
2023-01-19 19:12:03 -0500

    created Pipe class and Trip class

--- day_16_JS/Advent_16_1.js
+++ day_16_JS/Advent_16_1.js
@@ -3,8 +3,11 @@
 const fs = require('fs');
 
 let file_data;
+
 const file = "dummy.txt"
 // const file = "data.txt"
+
+
 fs.readFile(file, 'utf8', (err, data) => {
     if(err) throw err;
     file_data = data.split("\r\n")
@@ -24,26 +27,98 @@ fs.readFile(file, 'utf8', (err, data) => {
         }
     }
 
-    console.log(pipes['AA'])
+    const trip = new Trip(pipes, "AA")
+
+    print(pipes["AA"].traverse())
+
 });
 
+let pipecount = 0
 class Pipe {
     constructor(name, rate){
+        pipecount += 1
+        this.id = pipecount
         this.name = name;
         this.rate = rate;
         this.outs = [];
         this.is_open = false;
+        this.released = 0;
     }
 
     open(){
         this.is_open = true;
     }
 
+    step(){
+        if(this.is_open){this.released += this.rate}
+    }
 
+    traverse(visited=new Set(), step=1){
+        const head = this
+        let value = 0;
+        let total_released = 0;
+        let total_rate = 0;
+        let steps = 0;
+        
+        if(visited.has(head.name)){
+            print(`Already visited ${head.name}`)
+            return 0;
+        }
+        visited.add(head.name)
+        value += head.rate
+        print(`Visit ${head.name} in ${step} steps -> ${value}`)
+        
+        for(const out of head.outs){
+            // if(!visited.has(out.name)){
+                // print(`${out.name} adds ${out.rate}`)
+                value += out.traverse(visited, step + 1)
+            // }
+        }
+        
+        return value
+    }
+}
+
+class Trip {
+    constructor(graph, head){
+        this.graph = graph
+        this.head = graph[head]
+        this.total_released = 0; 
+        this.minutes = 0; 
+        this.total_rate = 0;
+        this.total_opened = 0; 
+        this.tunnels_traveled = 0;
+        this.nodes_visited = new Set()
+    }
+
+    step_to(node){
+        this.head = node;
+        this.minutes++;
+        this.tunnels_traveled++;
+        this.nodes_visited.add(this.head.name)
+        this.total_released += this.total_rate
+    }
+
+    open_head(){
+        if(this.head.is_open){return}
+        this.head.open();
+        this.total_rate += this.head.rate;
+        this.minutes++;
+        this.total_opened++;
+    }
+
+    distance_to(pipe){
+        let distance = this.head.traverse("CC").distance
+
+    }
 
 
 }
 
+
+function print(x){
+    console.log(x)
+}
 
 // function DFS(head){
 //     const start = head;
